0374ec1e94f5d1b0d87b1b9c8d7ca0fc820e2bf0
José Carlos López <jcl.isc@gmail.com>
2021-04-28 09:00:51 -0700

    Version v0.2.1
    - Added small disclaimer on the bottom-right corner

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,11 +5,14 @@ import MenuCard from "../components/MenuCard";
 export default function Home() {
   return (
     <div className="h-screen flex flex-col justify-center items-center">
-      <AppTitle text="Abecedario Interactivo" className="mb-8"/>
+      <AppTitle text="Abecedario Interactivo" className="mb-8" />
       <div className="flex space-x-12">
-        <MenuCard text="Sonidos" imgUrl="/images/volume.png" href="/soundboard"/>
-        <MenuCard text="Trazos" imgUrl="/images/pencil.png" href="/letter-strokes"/>
+        <MenuCard text="Sonidos" imgUrl="/images/volume.png" href="/soundboard" />
+        <MenuCard text="Trazos" imgUrl="/images/pencil.png" href="/letter-strokes" />
       </div>
+      <p className="fixed bottom-0 right-0 p-2 text-xs text-blue-700 font-bold">
+        <a href="https://www.linkedin.com/in/jclisc/">coded with ❤ by️ crls</a>
+      </p>
     </div>
   );
 }
